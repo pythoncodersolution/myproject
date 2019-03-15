ffd335c73c9be084702690016bc1006661470916
S M Russel <desertroseboy@gmail.com>
2019-03-15 20:55:03 +0600

    forms.py crated and created a form and added method to views.py to add , complete and delelte functionality

--- myproject/settings.py
+++ myproject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'todo',
+    'todo.apps.TodoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
